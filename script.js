9aa55b4b90329dec44292536cfe82eca0b6f3e76
= <postagestamp@windowslive.com>
2024-11-18 16:36:35 +1300

    Yep

---
+++ script.js
@@ -0,0 +1,5 @@
+function fuckYeah() {
+    document.getElementById('message').innerText = 'You\'re beautiful';
+    document.getElementById('message').hidden = false;
+    document.getElementById('startButton').hidden = true;
+}
